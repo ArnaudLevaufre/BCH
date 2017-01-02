b46f13bf43a5798a1a992130f5bffa7c12e307e7
Arnaud Levaufre <arnaud@bananium.fr>
2017-01-02 18:14:53 +0100

    Fix main.c indentation and print messages

--- quartus/software/BCH/main.c
+++ quartus/software/BCH/main.c
@@ -16,54 +16,53 @@ void BCH_ISR(void*);
 int main()
 {
 
-  alt_ic_isr_register(BCH_0_IRQ_INTERRUPT_CONTROLLER_ID, BCH_0_IRQ, BCH_ISR, NULL, NULL);
-  printf("Hello from Nios II!\n");
+    alt_ic_isr_register(BCH_0_IRQ_INTERRUPT_CONTROLLER_ID, BCH_0_IRQ, BCH_ISR, NULL, NULL);
+    printf("Hello from Nios II!\n");
 
-  volatile unsigned int status = BCH_read_status(BCH_0_BASE);
-  printf("Status: 0x%x\n", status);
+    volatile unsigned int status = BCH_read_status(BCH_0_BASE);
+    printf("Status: 0x%x\n", status);
 
-  volatile unsigned int ctrl = BCH_read_ctrl(BCH_0_BASE);
-  printf("Control: 0x%x\n", ctrl);
+    volatile unsigned int ctrl = BCH_read_ctrl(BCH_0_BASE);
+    printf("Control: 0x%x\n", ctrl);
 
-  // 0 Error
-  BCH_push_msg(BCH_0_BASE, 0x435A0BD5);
-  status = BCH_read_status(BCH_0_BASE);
-  printf("Status: 0x%x\n", status);
+    // 0 Error
+    BCH_push_msg(BCH_0_BASE, 0x435A0BD5);
+    status = BCH_read_status(BCH_0_BASE);
+    printf("Status: 0x%x\n", status);
 
-  // 1 Error
-  BCH_push_msg(BCH_0_BASE, 0x435A0BD4);
-  status = BCH_read_status(BCH_0_BASE);
-  printf("Status: 0x%x\n", status);
+    // 1 Error
+    BCH_push_msg(BCH_0_BASE, 0x435A0BD4);
+    status = BCH_read_status(BCH_0_BASE);
+    printf("Status: 0x%x\n", status);
 
-  // 2 Error
-  BCH_push_msg(BCH_0_BASE, 0x435A0AD4);
-  status = BCH_read_status(BCH_0_BASE);
-  printf("Status: 0x%x\n", status);
+    // 2 Error
+    BCH_push_msg(BCH_0_BASE, 0x435A0AD4);
+    status = BCH_read_status(BCH_0_BASE);
+    printf("Status: 0x%x\n", status);
 
-  // 3 Error
-  BCH_push_msg(BCH_0_BASE, 0x435A1AD4);
-  status = BCH_read_status(BCH_0_BASE);
-  printf("Status 1: 0x%x\n", status);
+    // 3 Error
+    BCH_push_msg(BCH_0_BASE, 0x435A1AD4);
+    status = BCH_read_status(BCH_0_BASE);
+    printf("Status 1: 0x%x\n", status);
 
-  BCH_enable_irq(BCH_0_BASE);
+    BCH_enable_irq(BCH_0_BASE);
 
-  ctrl = BCH_read_ctrl(BCH_0_BASE);
-  printf("Control 1: 0x%x\n", ctrl);
+    ctrl = BCH_read_ctrl(BCH_0_BASE);
+    printf("Control 1: 0x%x\n", ctrl);
 
-  BCH_start(BCH_0_BASE);
+    BCH_start(BCH_0_BASE);
 
-  //status = BCH_read_status(BCH_0_BASE);
-  //printf("Status 2: 0x%x\n", status);
+    //status = BCH_read_status(BCH_0_BASE);
+    //printf("Status 2: 0x%x\n", status);
 
-  while(!BCH_is_empty(BCH_0_BASE)){
-	  printf("waiting for empty BCH \n");
-  }
-  status = BCH_read_status(BCH_0_BASE);
-  printf("Status 3: 0x%x\n", status);
-  //usleep(4000000);
-  printf("First pass with irqs enabled done!\n");
+    while(!BCH_is_empty(BCH_0_BASE)){
+        printf("waiting for empty BCH \n");
+    }
+    status = BCH_read_status(BCH_0_BASE);
+    printf("Status 3: 0x%x\n", status);
+    printf("First pass with irqs enabled done!\n");
 
-  // 0 Error
+    // 0 Error
     BCH_push_msg(BCH_0_BASE, 0x435A0BD5);
     status = BCH_read_status(BCH_0_BASE);
     printf("Status: 0x%x\n", status);
@@ -98,49 +97,48 @@ int main()
     status = BCH_read_status(BCH_0_BASE);
     printf("Status: 0x%x\n", status);
 
-	bch_msg data;
-	BCH_pop_msg(BCH_0_BASE, &data);
-	status = BCH_read_status(BCH_0_BASE);
-	ctrl = BCH_read_ctrl(BCH_0_BASE);
-	printf("Error 0: 0x%X, Data: 0x%X; Status: 0x%X; Ctrl: 0x%X\n", data.error, data.msg, status, ctrl);
-	
+    bch_msg data;
     BCH_pop_msg(BCH_0_BASE, &data);
-	status = BCH_read_status(BCH_0_BASE);
-	ctrl = BCH_read_ctrl(BCH_0_BASE);
-	printf("Error 1: 0x%X, Data: 0x%X; Status: 0x%X; Ctrl: 0x%X\n", data.error, data.msg, status, ctrl);
-	
+    status = BCH_read_status(BCH_0_BASE);
+    ctrl = BCH_read_ctrl(BCH_0_BASE);
+    printf("Error: 0x%X, Data: 0x%X; Status: 0x%X; Ctrl: 0x%X\n", data.error, data.msg, status, ctrl);
+
     BCH_pop_msg(BCH_0_BASE, &data);
-	status = BCH_read_status(BCH_0_BASE);
-	ctrl = BCH_read_ctrl(BCH_0_BASE);
-	printf("Error 2: 0x%X, Data: 0x%X; Status: 0x%X; Ctrl: 0x%X\n", data.error, data.msg, status, ctrl);
-	
+    status = BCH_read_status(BCH_0_BASE);
+    ctrl = BCH_read_ctrl(BCH_0_BASE);
+    printf("Error: 0x%X, Data: 0x%X; Status: 0x%X; Ctrl: 0x%X\n", data.error, data.msg, status, ctrl);
+
     BCH_pop_msg(BCH_0_BASE, &data);
-	status = BCH_read_status(BCH_0_BASE);
-	ctrl = BCH_read_ctrl(BCH_0_BASE);
-	printf("Error 3: 0x%X, Data: 0x%X; Status: 0x%X; Ctrl: 0x%X\n", data.error, data.msg, status, ctrl);
+    status = BCH_read_status(BCH_0_BASE);
+    ctrl = BCH_read_ctrl(BCH_0_BASE);
+    printf("Error: 0x%X, Data: 0x%X; Status: 0x%X; Ctrl: 0x%X\n", data.error, data.msg, status, ctrl);
+
+    BCH_pop_msg(BCH_0_BASE, &data);
+    status = BCH_read_status(BCH_0_BASE);
+    ctrl = BCH_read_ctrl(BCH_0_BASE);
+    printf("Error: 0x%X, Data: 0x%X; Status: 0x%X; Ctrl: 0x%X\n", data.error, data.msg, status, ctrl);
     return 0;
 }
 
 void BCH_ISR(void* context){
-	bch_msg data;
-	BCH_pop_msg(BCH_0_BASE, &data);
-	unsigned int status = BCH_read_status(BCH_0_BASE);
-	unsigned int ctrl = BCH_read_ctrl(BCH_0_BASE);
-	printf("Error ISR 0: 0x%X, Data: 0x%X; Status: 0x%X; Ctrl: 0x%X\n", data.error, data.msg, status, ctrl);
-	BCH_pop_msg(BCH_0_BASE, &data);
-	status = BCH_read_status(BCH_0_BASE);
-	ctrl = BCH_read_ctrl(BCH_0_BASE);
-	printf("Error ISR 1: 0x%X, Data: 0x%X; Status: 0x%X; Ctrl: 0x%X\n", data.error, data.msg, status, ctrl);
-	BCH_pop_msg(BCH_0_BASE, &data);
-	status = BCH_read_status(BCH_0_BASE);
-	ctrl = BCH_read_ctrl(BCH_0_BASE);
-	printf("Error ISR 2: 0x%X, Data: 0x%X; Status: 0x%X; Ctrl: 0x%X\n", data.error, data.msg, status, ctrl);
-	BCH_pop_msg(BCH_0_BASE, &data);
-	status = BCH_read_status(BCH_0_BASE);
-	ctrl = BCH_read_ctrl(BCH_0_BASE);
-	printf("Error ISR 3: 0x%X, Data: 0x%X; Status: 0x%X; Ctrl: 0x%X\n", data.error, data.msg, status, ctrl);
-	//flush serial for display purposes
+    bch_msg data;
+    BCH_pop_msg(BCH_0_BASE, &data);
+    unsigned int status = BCH_read_status(BCH_0_BASE);
+    unsigned int ctrl = BCH_read_ctrl(BCH_0_BASE);
+    printf("Error 1: 0x%X, Data: 0x%X; Status: 0x%X; Ctrl: 0x%X\n", data.error, data.msg, status, ctrl);
+    BCH_pop_msg(BCH_0_BASE, &data);
+    status = BCH_read_status(BCH_0_BASE);
+    ctrl = BCH_read_ctrl(BCH_0_BASE);
+    printf("Error 2: 0x%X, Data: 0x%X; Status: 0x%X; Ctrl: 0x%X\n", data.error, data.msg, status, ctrl);
+    BCH_pop_msg(BCH_0_BASE, &data);
+    status = BCH_read_status(BCH_0_BASE);
+    ctrl = BCH_read_ctrl(BCH_0_BASE);
+    printf("Error 3: 0x%X, Data: 0x%X; Status: 0x%X; Ctrl: 0x%X\n", data.error, data.msg, status, ctrl);
+    BCH_pop_msg(BCH_0_BASE, &data);
+    status = BCH_read_status(BCH_0_BASE);
+    ctrl = BCH_read_ctrl(BCH_0_BASE);
+    printf("Error 4: 0x%X, Data: 0x%X; Status: 0x%X; Ctrl: 0x%X\n", data.error, data.msg, status, ctrl);
+    //flush serial for display purposes
     fflush(stdout);
-
 }
 
